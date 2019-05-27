9d6cac4a22e393d09da5d122ffe9b779d2a3c9f0
James <jordunjames@gmail.com>
2019-05-27 13:14:11 +0100

    initial commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    if (this.props.data) {
+      var networks = this.props.data.social.map(function (network) {
+        return (
+          <li key={network.name}>
+            <a href={network.url}>
+              <i className={network.className} />
+            </a>
+          </li>
+        );
+      });
+    }
+
+    return (
+      <footer>
+        <div className="row">
+          <div className="twelve columns">
+            <ul className="social-links">{networks}</ul>
+
+            <ul className="copyright">
+              <li>&copy; Copyright 2019 Ikwechegh James</li>
+              <li>Design by Jaycode</li>
+            </ul>
+          </div>
+          <div id="go-top">
+            <a className="smoothscroll" title="Back to Top" href="#home">
+              <i className="icon-up-open" />
+            </a>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
